e57aee62de10f10c32b1fbe52bb52495e51ed77e
Juan Molfese <70412807+JuanMolfese@users.noreply.github.com>
2022-04-20 15:14:13 -0300

    se cambia el tipo de dato de salida e implementa nueva formula de calculo.

--- practico_2/ej3/Main.java
+++ practico_2/ej3/Main.java
@@ -6,21 +6,11 @@ public class Main {
     public static void main(String[] args) {
 
         int num_ingresado = 8;
-        String bin = "";
-        System.out.println("El valor "+num_ingresado+" en notacion binaria es equivalente a "+ pasarBinario(num_ingresado,bin));
+        System.out.println("El valor "+num_ingresado+" en notacion binaria es equivalente a "+ pasarBinario(num_ingresado));
     }
 
-    private static String pasarBinario(int num, String bin){
-        if (num>0){
-            if(num%2==0){
-                bin+="0";
-                return pasarBinario(num/2,bin);
-            }
-            else{
-                bin+="1";
-                return pasarBinario(num/2,bin);
-            }
-        }
-        return bin;
+    private static String pasarBinario(int num){
+        if (num<2)return num;
+        else return num%2+pasarBinario(num/2)*10;
     }
 }
