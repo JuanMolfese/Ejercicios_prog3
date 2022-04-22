5586cbf9260e8d978e77292e64dd232c9d863eed
JuanMolfese <juanjosemolfese@gmail.com>
2022-04-22 14:03:13 -0300

    Fusiono ordenar y mostrar arreglos ej 4.

---
+++ practico_2/ej4/Burbujeo.java
@@ -0,0 +1,30 @@
+package practico_2.ej4;
+//metodo burbujeo adaptativo. controla que en la primer pasada ya este ordenado.
+
+public class Burbujeo {
+    public Burbujeo(int[] arreglo) {
+
+    }
+
+    public void ordenar(int[] arreglo) {
+        boolean intercambiado = true;
+        int j = 0;
+        int tmp;
+        while (intercambiado) {
+            intercambiado = false;
+            j++;
+            for (int i = 0; i < arreglo.length - j; i++) {
+                if (arreglo[i] > arreglo[i + 1]) {
+                    tmp = arreglo[i];
+                    arreglo[i] = arreglo[i + 1];
+                    arreglo[i + 1] = tmp;
+                    intercambiado = true;
+                }
+            }
+        }
+        for (int r = 0; r < arreglo.length; r++) {
+            System.out.print(arreglo[r] + "    ");
+        }
+        System.out.println("");
+    }
+}
--- practico_2/ej4/Main.java
+++ practico_2/ej4/Main.java
@@ -6,5 +6,11 @@ package practico_2.ej4;
 public class Main {
     public static void main(String[] args) {
 
+        int[] arreglo = new int[]{ 2,10,3,8,4,6,7,5,9,1,20,17 };
+        int[] arreglo2 = new int[]{ 1,3,4,5,6,7,8,9,10 };
+        Burbujeo b = new Burbujeo(arreglo);
+        Burbujeo b_2 = new Burbujeo(arreglo2);
+        b.ordenar(arreglo);
+        b_2.ordenar(arreglo2);
     }
 }
